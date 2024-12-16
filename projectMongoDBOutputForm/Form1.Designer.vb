ebc5df1195d5e246f8ce5235c34a004153135bc9
jasaga2401 <jasaga@protonmail.com>
2024-12-16 22:22:52 +0000

    Add project files.

---
+++ projectMongoDBOutputForm/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblForename = New System.Windows.Forms.Label()
+        Me.lblSurname = New System.Windows.Forms.Label()
+        Me.txtTitle = New System.Windows.Forms.TextBox()
+        Me.txtForename = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.lblTitleForm = New System.Windows.Forms.Label()
+        Me.btnPrevious = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.lblRecordIndex = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(143, 165)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(61, 29)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "Title"
+        '
+        'lblForename
+        '
+        Me.lblForename.AutoSize = True
+        Me.lblForename.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblForename.Location = New System.Drawing.Point(143, 216)
+        Me.lblForename.Name = "lblForename"
+        Me.lblForename.Size = New System.Drawing.Size(124, 29)
+        Me.lblForename.TabIndex = 1
+        Me.lblForename.Text = "Forename"
+        '
+        'lblSurname
+        '
+        Me.lblSurname.AutoSize = True
+        Me.lblSurname.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSurname.Location = New System.Drawing.Point(143, 268)
+        Me.lblSurname.Name = "lblSurname"
+        Me.lblSurname.Size = New System.Drawing.Size(110, 29)
+        Me.lblSurname.TabIndex = 2
+        Me.lblSurname.Text = "Surname"
+        '
+        'txtTitle
+        '
+        Me.txtTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTitle.Location = New System.Drawing.Point(301, 162)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(227, 30)
+        Me.txtTitle.TabIndex = 3
+        '
+        'txtForename
+        '
+        Me.txtForename.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtForename.Location = New System.Drawing.Point(301, 213)
+        Me.txtForename.Name = "txtForename"
+        Me.txtForename.Size = New System.Drawing.Size(227, 30)
+        Me.txtForename.TabIndex = 4
+        '
+        'txtSurname
+        '
+        Me.txtSurname.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSurname.Location = New System.Drawing.Point(301, 265)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(227, 30)
+        Me.txtSurname.TabIndex = 5
+        '
+        'lblTitleForm
+        '
+        Me.lblTitleForm.AutoSize = True
+        Me.lblTitleForm.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitleForm.Location = New System.Drawing.Point(296, 101)
+        Me.lblTitleForm.Name = "lblTitleForm"
+        Me.lblTitleForm.Size = New System.Drawing.Size(76, 29)
+        Me.lblTitleForm.TabIndex = 6
+        Me.lblTitleForm.Text = "Users"
+        '
+        'btnPrevious
+        '
+        Me.btnPrevious.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPrevious.Location = New System.Drawing.Point(301, 326)
+        Me.btnPrevious.Name = "btnPrevious"
+        Me.btnPrevious.Size = New System.Drawing.Size(107, 36)
+        Me.btnPrevious.TabIndex = 7
+        Me.btnPrevious.Text = "Previous"
+        Me.btnPrevious.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNext.Location = New System.Drawing.Point(421, 326)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(107, 36)
+        Me.btnNext.TabIndex = 8
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'lblRecordIndex
+        '
+        Me.lblRecordIndex.AutoSize = True
+        Me.lblRecordIndex.Location = New System.Drawing.Point(578, 339)
+        Me.lblRecordIndex.Name = "lblRecordIndex"
+        Me.lblRecordIndex.Size = New System.Drawing.Size(87, 16)
+        Me.lblRecordIndex.TabIndex = 9
+        Me.lblRecordIndex.Text = "Record count"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblRecordIndex)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.btnPrevious)
+        Me.Controls.Add(Me.lblTitleForm)
+        Me.Controls.Add(Me.txtSurname)
+        Me.Controls.Add(Me.txtForename)
+        Me.Controls.Add(Me.txtTitle)
+        Me.Controls.Add(Me.lblSurname)
+        Me.Controls.Add(Me.lblForename)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblForename As Label
+    Friend WithEvents lblSurname As Label
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents txtForename As TextBox
+    Friend WithEvents txtSurname As TextBox
+    Friend WithEvents lblTitleForm As Label
+    Friend WithEvents btnPrevious As Button
+    Friend WithEvents btnNext As Button
+    Friend WithEvents lblRecordIndex As Label
+End Class
---
+++ projectMongoDBOutputForm/Form1.vb
@@ -0,0 +1,75 @@
+﻿
+Imports MongoDB.Driver
+Imports MongoDB.Bson
+Public Class Form1
+
+
+    ' MongoDB Connection String
+    Private connectionString As String = "mongodb+srv://jasaga:igywFraEIYcgiMZG@cluster0.sd0fg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+    Private client As MongoClient
+    Private database As IMongoDatabase
+    Private collection As IMongoCollection(Of BsonDocument)
+    Private documents As List(Of BsonDocument)
+    Private currentIndex As Integer = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Try
+            ' Initialize MongoDB client and access collection
+            client = New MongoClient(connectionString)
+            database = client.GetDatabase("dbcustomer")
+            collection = database.GetCollection(Of BsonDocument)("users")
+
+            ' Load all documents into a list
+            documents = collection.Find(New BsonDocument()).ToList()
+
+            If documents.Count > 0 Then
+                DisplayRecord(currentIndex)
+            Else
+                MessageBox.Show("No records found.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub DisplayRecord(index As Integer)
+
+        If index >= 0 And index < documents.Count Then
+
+            Dim doc = documents(index)
+            txtTitle.Text = doc.GetValue("title", "").AsString
+            txtForename.Text = doc.GetValue("forename", "").AsString
+            txtSurname.Text = doc.GetValue("surname", "").AsString
+            lblRecordIndex.Text = $"Record {index + 1} of {documents.Count}"
+
+        End If
+
+    End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+
+        If currentIndex < documents.Count - 1 Then
+            currentIndex += 1
+            DisplayRecord(currentIndex)
+        Else
+            MessageBox.Show("You are at the last record.")
+        End If
+
+    End Sub
+
+
+    Private Sub btnPrevious_Click(sender As Object, e As EventArgs) Handles btnPrevious.Click
+
+        If currentIndex > 0 Then
+            currentIndex -= 1
+            DisplayRecord(currentIndex)
+        Else
+            MessageBox.Show("You are at the first record.")
+        End If
+
+    End Sub
+
+End Class
